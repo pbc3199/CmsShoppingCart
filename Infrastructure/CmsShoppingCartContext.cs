4724bd53d5d3714be7cf51d6fc793e0f45747587
faizal.sulaiman@teras.uemnet.com <faizal.sulaiman@teras.uemnet.com>
2020-04-14 11:59:43 +0800

    Add project files.

---
+++ Infrastructure/CmsShoppingCartContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsShoppingCart.Infrastructure
+{
+    public class CmsShoppingCartContext : DbContext
+    {
+        public CmsShoppingCartContext(DbContextOptions<CmsShoppingCartContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
